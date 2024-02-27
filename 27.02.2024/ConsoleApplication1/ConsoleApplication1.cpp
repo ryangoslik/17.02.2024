74efc3258f10eee78ac99ae01e5b909fb23a9855
pawelce <szkolacecot@gmail.com>
2024-02-27 09:52:12 +0100

    111

---
+++ 27.02.2024/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,65 @@
+﻿
+#include <iostream>
+
+using namespace std;
+
+class Samochody {
+private:
+	string marka;
+	string kolor;
+	float pojemnosc;
+	float cena;
+	int przebieg;
+public:
+	Samochody(string m, string k, float poj, float c, int przeb) {
+		this->marka = m;
+		this->kolor = k;
+		this->pojemnosc = poj;
+		this->cena = c;
+		this->przebieg = przeb;
+	}
+	Samochody(string m, string k) {
+		this->marka = m;
+		this->kolor = k;
+		this->prezentacjaSamochodu2();
+	} 
+	void prezentacjaSamochodu() {
+			setlocale(LC_ALL, "");
+			cout << " Marka: " << this->marka << "\n";
+			cout << " kolor: " << this->kolor << "\n";
+			cout << " Pojemnosc: " << this->pojemnosc << "\n";
+			cout << " cena: " << this->cena << "\n";
+			cout << " przebieg: " << this->przebieg << "\n";
+			cout << "----------------------------------------------" << endl;
+		
+	}
+	void prezentacjaSamochodu2() {
+		setlocale(LC_ALL, "");
+		cout << "##############################################" << endl;
+		cout << " Marka: " << this->marka << "\n";
+		cout << " kolor: " << this->kolor << "\n";
+		cout << "----------------------------------------------" << endl;
+	}
+};
+int main()
+{
+	//Samochody fiat = Samochody("FIAT", "Kupa", 1.0, 250.56756565, 180);
+	//fiat.prezentacjaSamochodu();
+	string marka[3] = { "Fiat", "Volvo", "Skoda" };
+	string kolor[3] = { "green", "blue" , "black" };
+	float pojemnosc[3] = { 1.2,1.8,2.4 };
+	float cena[3] = { 180,250,244 };
+	int przebieg[3] = { 1,10,8 };
+
+	for (int i = 0; i < 3; i++) {
+		Samochody sam = Samochody(marka[i], kolor[i], pojemnosc[i], cena[i], przebieg[i]);
+		sam.prezentacjaSamochodu();
+	};
+
+	for (int i = 0; i < 3; i++) {
+		Samochody samochod = Samochody(marka[i], kolor[i]);
+
+	};
+
+	return EXIT_SUCCESS; 
+}
---
+++ 27.02.2024/ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,60 @@
+﻿#include <iostream>
+#include <math.h>
+using namespace std;
+
+class Ksztalty {
+private:
+	const float pi = 3.14;
+	float r;
+	float h;
+	float a;
+	float b;
+	float e, f;
+public:
+	Ksztalty(float r1, float h1, float a1, float b1, float e1, float f1) {
+		
+		this->r = r1;
+		this->h = h1;
+		this->a = a1;
+		this->b = b1;
+		this->e = e1;
+		this->f = f1;
+
+	}
+	void kolo() {
+		cout << "Pole kola: ";
+		cout << this->pi * pow(this->r, 2) << endl;
+	}
+	void kwadrat() {
+		cout << "Pole kwadratu: ";
+		cout << this->a * this->b << endl;
+	}
+	void trojkat() {
+		cout << "Pole trojkata: ";
+		cout << (this->a * this->h)/2 << endl;
+	}
+	
+	
+	void romb() {
+		cout << "Pole romb: ";
+		cout << (this->e * this->f) / 2 << endl;;
+	}
+
+};
+int main()
+{
+	setlocale(LC_ALL, "");
+	float r, h, a, b, e, f;
+	cout << "podaj r:"; cin >> r;
+	cout << "podaj h:"; cin >> h;
+	cout << "podaj a:"; cin >> a;
+	cout << "podaj b:"; cin >> b;
+	cout << "podaj e:"; cin >> e;
+	cout << "podaj f:"; cin >> f;
+	Ksztalty nr1(r, h, a, b, e, f);
+	nr1.kolo();
+	nr1.kwadrat();
+	nr1.romb();
+	nr1.trojkat();
+	return EXIT_SUCCESS;
+}
